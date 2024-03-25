e94a932544580be68c7f578bfa60f7a29daa8aaa
raghav3102 <raghav3102@gmail.com>
2024-03-25 12:39:47 +0530

    first commit

---
+++ service/common/__init__.py
@@ -0,0 +1,9 @@
+"""
+Utility package
+
+This package contains utility code that is not part
+of any particular application
+"""
+from .log_handlers import init_logging
+
+__all__ = ('init_logging',)
